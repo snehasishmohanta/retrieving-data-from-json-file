10d0be6678426b0b0f4ea73449617570485adc93
snehasishmohanta <snehasishmohanta@gmail.com>
2015-01-09 15:28:24 +0530

    Update and rename .gitignore to ext_json.py

---
+++ ext_json.py
@@ -0,0 +1,46 @@
+"""
+     
+      {"requestDate":"2014-12-17 00:00:06.308","taskId":"558b64a4-014a-1000-8006-970e8333b0c9","appData":{"user_ip":"182.65.111.114, 10.0.255.47","_request_params":{"params":{"uid":["f0c831c3-d0b6-4e19-d813-3d7421a41ad5"],"loadtime":["6057"],"filtered":["false"],"requestId":["558b4d15-014a-1000-802c-9e74d9e49d4e"],"bId":["control"],"userid":["3c9b42f9-29b8-4db7-bd44-281e32d770cc"],"page":["1"],"q":["htc beats"],"eId":["control"],"action":["search"],"skus":["1143810,1143810,4049024,4049024,1762986,1762986,4699248,4699248,3344088,3344088,4218624,4218624,1389332,1389332,296862,296862,4218848,4218848,3030558,3030558,6599302,6599302,2206162,2206162,2193662,2193662,2279932,2279932,1483056,1483056,5130096,5130096,1232316,1232316,2193348,2193348,1763084,1763084,1346522,1346522,1468182,1468182,1645516,1645516,6598762,6598762,6627826,6627826,5130126,5130126,6233252,6233252,5153848,5153848,5058602,5058602,6504014,6504014,6540420,6540420,6506490,6506490,6551444,6551444,6503934,6503934,6546746,6546746,6503978,6503978,6488504,6488504"],"side_cat":["true"],"z":["1"]},"headers":{"accept-language":"en-US,en;q\u003d0.5","host":"api.apac-sg.zettata.com","x-forwarded-for":"182.65.111.114, 10.0.255.47","x-forwarded-port":"80","user-agent":"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0","x-forwarded-proto":"http","x-real-ip":"10.0.255.47","referer":"http://www.shopclues.com/index.php?subcats\u003dY\u0026status\u003dA\u0026pname\u003dY\u0026product_code\u003dY\u0026match\u003dall\u0026pkeywords\u003dY\u0026search_performed\u003dY\u0026z\u003d1\u0026q\u003dhtc+beats\u0026cid\u003d0\u0026dispatch\u003dproducts.search\u0026page\u003d2","accept-encoding":"gzip, deflate"},"inCookies":[],"post":false},"cookies":[],"request_params":{"params":{"uid":["f0c831c3-d0b6-4e19-d813-3d7421a41ad5"],"loadtime":["6057"],"filtered":["false"],"requestId":["558b4d15-014a-1000-802c-9e74d9e49d4e"],"bId":["control"],"userid":["3c9b42f9-29b8-4db7-bd44-281e32d770cc"],"page":["1"],"q":["htc beats"],"eId":["control"],"action":["search"],"skus":["1143810,1143810,4049024,4049024,1762986,1762986,4699248,4699248,3344088,3344088,4218624,4218624,1389332,1389332,296862,296862,4218848,4218848,3030558,3030558,6599302,6599302,2206162,2206162,2193662,2193662,2279932,2279932,1483056,1483056,5130096,5130096,1232316,1232316,2193348,2193348,1763084,1763084,1346522,1346522,1468182,1468182,1645516,1645516,6598762,6598762,6627826,6627826,5130126,5130126,6233252,6233252,5153848,5153848,5058602,5058602,6504014,6504014,6540420,6540420,6506490,6506490,6551444,6551444,6503934,6503934,6546746,6546746,6503978,6503978,6488504,6488504"],"side_cat":["true"],"z":["1"]},"headers":{"accept-language":"en-US,en;q\u003d0.5","host":"api.apac-sg.zettata.com","x-forwarded-for":"182.65.111.114, 10.0.255.47","x-forwarded-port":"80","user-agent":"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0","x-forwarded-proto":"http","x-real-ip":"10.0.255.47","referer":"http://www.shopclues.com/index.php?subcats\u003dY\u0026status\u003dA\u0026pname\u003dY\u0026product_code\u003dY\u0026match\u003dall\u0026pkeywords\u003dY\u0026search_performed\u003dY\u0026z\u003d1\u0026q\u003dhtc+beats\u0026cid\u003d0\u0026dispatch\u003dproducts.search\u0026page\u003d2","accept-encoding":"gzip, deflate"},"inCookies":[],"post":false},"user_agent":"Mozilla/5.0 (Windows NT 6.3; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0"},"cookies":[],"status":200}
+
+
+    {"requestDate":"2014-12-17 08:00:00.33","taskId":"5742c021-014a-1000-800e-9e74d9e49d4e","appData":{"user_ip":"182.73.108.98, 10.0.255.47","_req
+    uest_params":{"params":{"uid":["f3469278-5af2-4918-e418-2ef4bb9174aa"],"loadtime":["5462"],"filtered":["false"],"sort_by":["hotdeals "],"fsrc":
+    ["By Type:By Type@facet%3A15092515296306"],"userid":["7c4703c0-feff-4e49-ab2d-b8af5e442700"],"page":["1"],"action":["search"],"requestId":["574
+    2ab28-014a-1000-8022-9e74d9e49d4e"],"bId":["cartrateserve_n2"],"q":["<U+200B><U+200B><U+200B>helmets"],"eId":["cartrateserve"],"fac":["By Type@
+    facet%3A15092515296306"],"skus":["1024200,1024200,1024170,1024170,6142516,6142516,6142646,6142646,1070748,1070748,1067392,1067392,6142756,61427
+    56,1067806,1067806,1112234,1112234,1074632,1074632,1071516,1071516,6142530,6142530,1071342,1071342,1151002,1151002,6142598,6142598,6142588,6142
+    588,1151006,1151006,1143494,1143494,1149194,1149194,1070868,1070868,1067724,1067724,1067896,1067896,6142624,6142624,1118616,1118616,6142688,614
+    2688,6142656,6142656,1067646,1067646,1119144,1119144,1028442,1028442,1067698,1067698,1067290,1067290,1708596,1708596,1144438,1144438,1083836,10
+    83836,1119422,1119422,1122274,1122274"],"side_cat":["true"],"sort_order":["desc"],"z":["1"]},"headers":{"x-msisdn":"919920294455","x-forwarded-
+    proto":"http","cache-control":"max-age\u003d259200","accept-encoding":"gzip, deflate","accept-language":"en-US,en;q\u003d0.5","host":"api.apac-
+    sg.zettata.com","x-forwarded-for":"182.73.108.98, 10.0.255.47","x-forwarded-port":"80","user-agent":"Mozilla/5.0 (Windows NT 6.2; WOW64; rv:34.
+    0) Gecko/20100101 Firefox/34.0","ip":"1.38.15.2","x-real-ip":"10.0.255.47","referer":"http://www.shopclues.com/index.php?search_performed\u003d
+    Y\u0026cid\u003d0\u0026q\u003d%E2%80%8B%E2%80%8B%E2%80%8Bhelmets\u0026search_performed\u003dY\u0026z\u003d1\u0026fac%5B%5D\u003dBy+Type%40facet
+    %253A15092515296314\u0026fac%5B%5D\u003dBy+Type%40facet%253A15092515296306\u0026brand_search\u003d\u0026company_search\u003d\u0026sp\u003d\u002
+    6bp\u003d50%2C12400\u0026sprice\u003d50\u0026eprice\u003d12400\u0026fsrc\u003dBy+Type%3ABy+Type%40facet%253A15092515296306\u0026dispatch\u003dp
+    roducts.search\u0026sort_by\u003dhotdeals\u0026sort_order\u003ddesc"},"inCookies":[],"post":false},"cookies":[],"request_params":{"params":{"ui
+    d":["f3469278-5af2-4918-e418-2ef4bb9174aa"],"loadtime":["5462"],"filtered":["false"],"sort_by":["hotdeals "],"fsrc":["By Type:By Type@facet%3A1
+    5092515296306"],"userid":["7c4703c0-feff-4e49-ab2d-b8af5e442700"],"page":["1"],"action":["search"],"requestId":["5742ab28-014a-1000-8022-9e74d9
+    e49d4e"],"bId":["cartrateserve_n2"],"q":["<U+200B><U+200B><U+200B>helmets"],"eId":["cartrateserve"],"fac":["By Type@facet%3A15092515296306"],"s
+    kus":["1024200,1024200,1024170,1024170,6142516,6142516,6142646,6142646,1070748,1070748,1067392,1067392,6142756,6142756,1067806,1067806,1112234,
+    1112234,1074632,1074632,1071516,1071516,6142530,6142530,1071342,1071342,1151002,1151002,6142598,6142598,6142588,6142588,1151006,1151006,1143494
+    ,1143494,1149194,1149194,1070868,1070868,1067724,1067724,1067896,1067896,6142624,6142624,1118616,1118616,6142688,6142688,6142656,6142656,106764
+    6,1067646,1119144,1119144,1028442,1028442,1067698,1067698,1067290,1067290,1708596,1708596,1144438,1144438,1083836,1083836,1119422,1119422,11222
+    74,1122274"],"side_cat":["true"],"sort_order":["desc"],"z":["1"]},"headers":{"x-msisdn":"919920294455","x-forwarded-proto":"http","cache-contro
+    l":"max-age\u003d259200","accept-encoding":"gzip, deflate","accept-language":"en-US,en;q\u003d0.5","host":"api.apac-sg.zettata.com","x-forwarde
+    d-for":"182.73.108.98, 10.0.255.47","x-forwarded-port":"80","user-agent":"Mozilla/5.0 (Windows NT 6.2; WOW64; rv:34.0) Gecko/20100101 Firefox/3
+    4.0","ip":"1.38.15.2","x-real-ip":"10.0.255.47","referer":"http://www.shopclues.com/index.php?search_performed\u003dY\u0026cid\u003d0\u0026q\u0
+    03d%E2%80%8B%E2%80%8B%E2%80%8Bhelmets\u0026search_performed\u003dY\u0026z\u003d1\u0026fac%5B%5D\u003dBy+Type%40facet%253A15092515296314\u0026fac%5B%5D\u003dBy+Type%40facet%253A15092515296306\u0026brand_search\u003d\u0026company_search\u003d\u0026sp\u003d\u0026bp\u003d50%2C12400\u0026sprice\u003d50\u0026eprice\u003d12400\u0026fsrc\u003dBy+Type%3ABy+Type%40facet%253A15092515296306\u0026dispatch\u003dproducts.search\u0026sort_by\u003dhotdeals\u0026sort_order\u003ddesc"},"inCookies":[],"post":false},"user_agent":"Mozilla/5.0 (Windows NT 6.2; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0"},"cookies":[],"status":200}
+
+"""
+  #above is the format for json file.
+  # below is the code to get the value
+  
+    import re
+    import json
+    import sys
+    file1 = sys.argv[1]
+    with open(file1) as text:
+            for line in text:
+                    data = json.loads(line)
+                    print data["requestDate"],data["appData"]["_request_params"]["params"]["q"]
